ac89bbb7d78d1770acb331666f3da83c35e845e5
HaoyuanDu <jasonduuuu@hotmail.com>
2021-03-08 21:58:38 -0500

    fixed the proxy and url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'http://https://rocky-lake-34125.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
